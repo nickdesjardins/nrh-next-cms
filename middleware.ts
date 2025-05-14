2d3c2652735eaa972c4a3bb109afbd679bd140cf
Nicolas Desjardins <nick.desjardins@live.com>
2025-05-14 12:56:33 -0400

    reverted

--- middleware.ts
+++ middleware.ts
@@ -116,5 +116,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|auth/.*|sign-in|sign-up|forgot-password|unauthorized|api/auth/.*).*)',
+    '/cms/:path*',
   ],
 };
