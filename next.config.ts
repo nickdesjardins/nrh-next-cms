0921cc26f3a18d420c1813a49de08d1ab6344810
Nicolas Desjardins <nick.desjardins@live.com>
2025-06-05 16:19:55 -0400

    Testing Github actions with env variables

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
